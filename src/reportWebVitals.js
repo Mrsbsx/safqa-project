62b4b74a0ac75bab8e740404f5413fef99a8f1c6
Mrsbsx <182701546+Mrsbsx@users.noreply.github.com>
2024-11-17 12:12:25 +0300

    إضافة الملفات الأولية لموقع صفقة

---
+++ src/reportWebVitals.js
@@ -0,0 +1,5 @@
+const reportWebVitals = (metric) => {
+  console.log(metric); // يمكنك استبدال هذا الكود بالأنظمة التي تراقب الأداء مثل Google Analytics أو غيرها
+};
+
+export default reportWebVitals;
